cf4e96749cd672bf601aab82382ab47e604cab10
DustinBrown917 <43157195+DustinBrown917@users.noreply.github.com>
2019-04-17 14:49:24 -0400

    modifications to gunthing

--- Project-Megaman/Assets/_Scripts/Enemies/GunThing.cs
+++ Project-Megaman/Assets/_Scripts/Enemies/GunThing.cs
@@ -11,6 +11,10 @@ namespace MEGA
         [SerializeField] private Vector3 projectileOffset;
         [SerializeField] private Vector2 projectileVelocity;
 
+        [SerializeField] private float initialShotAngle = 0.0f;
+        [SerializeField] private float shotAngleIncrement = 15.0f;
+        private int shotCount = 0;
+
         // Start is called before the first frame update
         protected override void Start()
         {
@@ -41,10 +45,13 @@ namespace MEGA
 
         protected override void FireProjectile()
         {
+            
             float flipFactor = (spriteRenderer.flipX) ? 1.0f : -1.0f;
             Vector2 projVel = new Vector2(projectileVelocity.x * flipFactor, projectileVelocity.y);
+            projVel = Vector2FromAngle(projVel, initialShotAngle + (shotAngleIncrement * shotCount));
             Instantiate(projectile, transform.position + (projectileOffset * flipFactor), Quaternion.identity).GetComponent<Projectile>().SetLaunchVelocity(projVel);
-            
+            shotCount++;
+            if (shotCount == 4) { shotCount = 0; }
         }
 
         protected override IEnumerator EnemyBehaviour()
@@ -56,6 +63,20 @@ namespace MEGA
         {
             base.IReset();
             animator.Play("GunThing_Idle");
+            shotCount = 0;
+        }
+
+        public Vector2 Vector2FromAngle(Vector2 v, float degrees)
+        {
+            float sin = Mathf.Sin(degrees * Mathf.Deg2Rad);
+            float cos = Mathf.Cos(degrees * Mathf.Deg2Rad);
+
+            float tx = v.x;
+            float ty = v.y;
+            v.x = (cos * tx) - (sin * ty);
+            v.y = (sin * tx) + (cos * ty);
+
+            return v;
         }
     }
 }
