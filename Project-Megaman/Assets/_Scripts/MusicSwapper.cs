1f29572fe6d8947e07bd1de4f1134ab84d147777
DustinBrown917 <43157195+DustinBrown917@users.noreply.github.com>
2019-04-18 19:01:52 -0400

    A2 done

---
+++ Project-Megaman/Assets/_Scripts/MusicSwapper.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicSwapper : MonoBehaviour
+{
+    [SerializeField] private AudioClip enterClip;
+    [SerializeField] private AudioClip exitClip;
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private GameObject triggerObject;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject == triggerObject)
+        {
+            audioSource.clip = enterClip;
+            audioSource.Play();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject == triggerObject)
+        {
+            audioSource.clip = exitClip;
+            audioSource.Play();
+        }
+    }
+}
